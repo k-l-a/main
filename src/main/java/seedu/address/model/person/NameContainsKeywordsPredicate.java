97dc50a115046a0bf6dcf3303590e94c76b9d111
k-l-a <k.l.a@u.nus.edu>
2017-10-19 19:47:23 +0800

    Modify Name predicate class to accept a boolOperator variable and update the behaviour of test method accordingly

--- src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/NameContainsKeywordsPredicate.java
@@ -1,31 +1,62 @@
 package seedu.address.model.person;
 
+import static seedu.address.model.person.PredicateSyntax.BOOLEAN_AND;
+import static seedu.address.model.person.PredicateSyntax.BOOLEAN_NOT;
+import static seedu.address.model.person.PredicateSyntax.BOOLEAN_OR;
+
 import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.StringUtil;
 
 /**
- * Tests that a {@code ReadOnlyPerson}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code ReadOnlyPerson}'s {@code Name} matches the keywords according to the
+ * boolean operator (AND, OR, NOT). If boolean operator is not one of those three values, defaults to OR.
  */
 public class NameContainsKeywordsPredicate implements Predicate<ReadOnlyPerson> {
     private final List<String> keywords;
+    private String boolOperator;
 
     public NameContainsKeywordsPredicate(List<String> keywords) {
+        //defaults to OR search
+        this(keywords, BOOLEAN_OR);
+    }
+
+    public NameContainsKeywordsPredicate(List<String> keywords, String boolOperator) {
         this.keywords = keywords;
+        if (isBoolOperator(boolOperator)) {
+            this.boolOperator = boolOperator;
+        } else {
+            this.boolOperator = BOOLEAN_OR;
+        }
+    }
+
+    private boolean isBoolOperator(String boolOperator) {
+        return boolOperator.equals(BOOLEAN_OR)
+                || boolOperator.equals(BOOLEAN_AND)
+                || boolOperator.equals(BOOLEAN_NOT);
     }
 
     @Override
     public boolean test(ReadOnlyPerson person) {
-        return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+        if (boolOperator.equals(BOOLEAN_AND)) {
+            return keywords.stream()
+                    .allMatch(keywords -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keywords));
+        } else if (boolOperator.equals(BOOLEAN_NOT)) {
+            return keywords.stream()
+                    .noneMatch(keywords -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keywords));
+        } else {
+            return keywords.stream()
+                    .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+        }
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof NameContainsKeywordsPredicate // instanceof handles nulls
-                && this.keywords.equals(((NameContainsKeywordsPredicate) other).keywords)); // state check
+                && this.keywords.equals(((NameContainsKeywordsPredicate) other).keywords)) // state check
+                && this.boolOperator.equals(((NameContainsKeywordsPredicate) other).boolOperator);
     }
 
 }
