88c36fdae25b4eb9b31786f65ac6aff263591151
k-l-a <k.l.a@u.nus.edu>
2017-10-19 19:46:24 +0800

    Add variables to PredicateSyntax class to be used for predicate classes and other classes dealing with them

--- src/main/java/seedu/address/model/person/PredicateSyntax.java
+++ src/main/java/seedu/address/model/person/PredicateSyntax.java
@@ -1,4 +1,10 @@
 package seedu.address.model.person;
 
+/**
+ * Contains syntax used by the predicate classes
+ */
 public class PredicateSyntax {
+    public static final String BOOLEAN_OR = "OR";
+    public static final String BOOLEAN_AND = "AND";
+    public static final String BOOLEAN_NOT = "NOT";
 }
